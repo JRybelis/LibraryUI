5b57f6f74304ed62f26e093447843c600eda1295
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-03-24 18:01:09 +0200

    create author & store to DB

--- libraryLaravelDb/routes/web.php
+++ libraryLaravelDb/routes/web.php
@@ -25,10 +25,10 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::group(['prefix' => 'authors'], function(){
     Route::get('',[AuthorController::class, 'index']) -> name ('author.index');
     Route::get('create', [AuthorController::class, 'create'])->name('author.create');
-    Route::post('store', [AuthorController::class, 'store']) -> name ('author.edit');
+    Route::post('store', [AuthorController::class, 'store']) -> name ('author.store');
     Route::get('edit/{author}', [AuthorController::class, 'edit']) -> name ('author.edit');
     Route::post('update/{author}', [AuthorController::class, 'update']) -> name('author.update');
     Route::post('delete/{author}', [AuthorController::class, 'destroy']) -> name('author.destroy');
     Route::get('show/{author}', [AuthorController::class, 'show'])-> name('author.show');
-
 });
+
